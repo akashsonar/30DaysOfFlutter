c0ea9ea819f691dd5f5c2b78e6fefa7cab32f58b
akash sonar <sonarakash888@gmail.com>
2022-03-31 12:48:11 +0530

    day 12

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
+import 'package:flutter_app/widgets/item_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-//learned about context and constraints.
+//learned about context and constraints
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,8 +21,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    var name = "Akash";
+    // int days = 30;
+    // var name = "Akash";
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -34,13 +35,15 @@ class HomePage extends StatelessWidget {
                 fontFamily: GoogleFonts.poppins().fontFamily),
           ),
         ),
-        body: Center(
-            child: Text(
-          "Welcome to $days days of Flutter with $name",
-          // ignore: prefer_const_constructors
-          style: const TextStyle(
-              color: Colors.teal, fontSize: 20, fontWeight: FontWeight.bold),
-        )),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ItemWidget(item: Item.fromJson(CatalogModel.items[index]),);
+            },
+          ),
+        ),
         drawer: const MyDrawer());
   }
 }
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/models/catalog.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item})
+      : assert(item != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print('${item.title} is pressed');
+        },
+        leading: Image.network(item.image.toString(),),
+        title: Text(item.title.toString()),
+        subtitle: Text(item.description.toString()),
+        trailing: Text(
+          "\$${item.price}",
+          style: TextStyle(
+              color: Colors.teal,
+              fontWeight: FontWeight.bold,
+              fontFamily: GoogleFonts.poppins().fontFamily),
+          textScaleFactor: 1.4,
+        ),
+      ),
+    );
+  }
+}
